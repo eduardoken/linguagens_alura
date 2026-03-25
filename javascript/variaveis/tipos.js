734993161d939b840ef2ca5a4e0ff24a5130a402
Eduardo Kenzo <edukenzo428@gmail.com>
2026-03-25 14:30:58 -0300

    tipos

---
+++ javascript/variaveis/tipos.js
@@ -0,0 +1,23 @@
+let nome = 'Ana'
+let idade = 23
+let altura = 1.67
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof altura)
+
+let estaChovendo = true
+
+console.log(typeof estaChovendo)
+
+let naoDefinido
+
+console.log(typeof naoDefinido)
+
+let aprendeuJS = null
+
+console.log(typeof aprendeuJS)
+
+let simbulo = Symbol()
+
+console.log(typeof simbulo)
