184e215ddf90f67d1b0042f2a2b5297f13ad8b81
Eduardo Kenzo <edukenzo428@gmail.com>
2026-03-25 14:36:05 -0300

    legibilidade

---
+++ javascript/variaveis/legibilidade.js
@@ -0,0 +1,14 @@
+let alturaPessoa = 1.89
+
+let altura = 7
+let largura = 10
+let area = 70
+
+let preco = 10
+let Preco = 20
+
+console.log('preco', preco)
+console.log('Preco', Preco)
+
+let idadeUsuario = 20
+let nomeUsuario = 'Eduardo'
