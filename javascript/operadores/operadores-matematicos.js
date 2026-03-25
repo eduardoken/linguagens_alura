6a4332808814dfcf8e8d064789146227dbd92fb6
Eduardo Kenzo <edukenzo428@gmail.com>
2026-03-25 15:43:05 -0300

    operadores

---
+++ javascript/operadores/operadores-matematicos.js
@@ -0,0 +1,43 @@
+const numeroUm = 4
+const numeroDois = 8
+
+const subtracao = numeroUm - numeroDois
+const soma = numeroUm + numeroDois
+const divisao = numeroUm / numeroDois
+const multiplicacao = numeroUm * numeroDois
+
+console.log('Subtração: ', subtracao)
+console.log('Soma: ', soma)
+console.log('Divisão: ', divisao)
+console.log('Multiplicação: ', multiplicacao)
+
+const contaComplexa = (numeroUm + numeroDois) * numeroDois
+
+console.log('Conta complexa: ', contaComplexa)
+
+const resto = 13 % 2
+
+console.log('Resto: ', resto)
+
+const potencia = 2 ** 3
+
+console.log('potência: ', potencia)
+
+let contador = 5
+
+// contador = contador + 1
+contador++
+
+console.log('Incremento: ', contador)
+
+contador--
+
+console.log('Decremento: ', contador)
+
+let numeroTres = 2
+
+numeroTres *= 2
+// numeroTres -= 2
+// numeroTres += 2
+// numeroTres /= 2
+console.log(numeroTres)
