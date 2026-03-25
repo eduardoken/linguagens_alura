472e2e63a5df0c3499381b8af2e3f71da89c7913
Eduardo Kenzo <edukenzo428@gmail.com>
2026-03-25 15:59:24 -0300

    comparacao

---
+++ javascript/operadores/operadores-comparacao.js
@@ -0,0 +1,16 @@
+const idade1 = '20'
+const idade2 = 20
+
+const comparacao1 = idade1 == idade2
+const comparacao2 = idade1 > idade2
+const comparacao3 = idade1 < idade2
+const comparacao4 = idade1 != idade2
+const comparacao5 = idade1 === idade2
+const comparacao6 = idade1 !== idade2
+
+console.log(comparacao1);
+console.log(comparacao2);
+console.log(comparacao3);
+console.log(comparacao4);
+console.log(comparacao5);
+console.log(comparacao6);
