bde5f2836f0719b0ce52f305e0b879318ed64db1
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-06 16:07:47 -0300

    adiciona variáveis para controle de tempo e banner na aplicação

--- javascript/dom/Fokus-projeto-base/script.js
+++ javascript/dom/Fokus-projeto-base/script.js
@@ -1,10 +1,15 @@
 const html = document.querySelector('html')
+const botaoIniciar = document.querySelector('.app__card-primary-button')
+const displayTempo = document.querySelector('#timer')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('./imagens/foco.png, ')
 })
 
 curtoBt.addEventListener('click', () => {
@@ -14,3 +19,7 @@ curtoBt.addEventListener('click', () => {
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
 })
+
+const duracaoFoco = 1500
+const descansoLongo = 900
+const descansoCurto = 300
