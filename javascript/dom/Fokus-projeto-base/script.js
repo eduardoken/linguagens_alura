95b44f07eb4f79842af9575b97b14d40d9b2f35c
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-06 18:21:19 -0300

    refatora a função alterarContexto para atualizar o banner e o título com base no contexto selecionado

--- javascript/dom/Fokus-projeto-base/script.js
+++ javascript/dom/Fokus-projeto-base/script.js
@@ -8,19 +8,46 @@ const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', './imagens/foco.png')
+    // html.setAttribute('data-contexto', 'foco')
+    // banner.setAttribute('src', './imagens/foco.png')
+    alterarContexto('foco')
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src','./imagens/descanso-curto.png')
+    //     html.setAttribute('data-contexto', 'descanso-curto')
+    //     banner.setAttribute('src','./imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src','./imagens/descanso-longo.png')
+    // html.setAttribute('data-contexto', 'descanso-longo')
+    // banner.setAttribute('src','./imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade, 
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada?
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+
+            break
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície. 
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+
+            break
+        default:
+            
+            break
+    }
+}
 
 const duracaoFoco = 1500
 const descansoLongo = 900
