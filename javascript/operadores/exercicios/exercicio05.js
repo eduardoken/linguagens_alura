24e784a86b50dd9b4a9fb172a06db993b2ee03cc
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-08 16:26:56 -0300

    exercicio 05

---
+++ javascript/operadores/exercicios/exercicio05.js
@@ -0,0 +1,11 @@
+const totalAulas = 9
+const faltas = 2
+const limiteFaltas = 9 * 25 / 100
+
+if (limiteFaltas < faltas) {
+    console.log('Limite de falta utrapassado')
+    
+} else {
+    console.log('Limite de faltas não utrapassado')
+    
+}
