b44ca820ea8435a1c087a48c4538b84bbdf3fe38
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-10 14:49:00 -0300

    operador ternario

--- javascript/condicoes_fluxo/condicionais.js
+++ javascript/condicoes_fluxo/condicionais.js
@@ -1,10 +1,33 @@
 const idade = 18
 
-if (idade >= 18){
-    console.log('É maior de idade')
+// if (idade >= 18){
+//     console.log('É maior de idade')
 
-} else {
-    console.log('É menor de idade')
+// } else {
+//     console.log('É menor de idade')
 
-}
+// }
 
+idade >= 18 ? console.log('Maior de idade') : console.log('Menor de idade')
+
+
+const notaDoAluno = 10
+
+// if (notaDoAluno >=9) {
+//     console.log('Nota excelente')
+    
+// } else if (notaDoAluno >=7) {
+//     console.log('Nota boa')
+    
+// } else if (notaDoAluno >=4) {
+//     console.log('Nota mediana')
+    
+// } else {
+//     console.log('Nota ruim')
+
+// }
+
+notaDoAluno >= 9 ? console.log('Nota excelente') :
+notaDoAluno >= 7 ? console.log('Nota boa') :
+notaDoAluno >= 4 ? console.log('Nota mediana') :
+console.log('Nota ruim') 
