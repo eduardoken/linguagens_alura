4eaecd16c5a52f4ba7d4856f134e52beca572166
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-10 15:36:00 -0300

    exercicio 04

--- javascript/condicoes_fluxo/exercicios/ex04.js
+++ javascript/condicoes_fluxo/exercicios/ex04.js
@@ -1,1 +1,14 @@
+const nota = 8
 
+if (nota >=9) {
+    console.log('Nota excelente')
+    
+} else if (nota >=6) {
+    console.log('Nota boa')
+    
+} else if (nota >= 4) {
+    console.log('Nota mediana')
+    
+} else {
+    console.log('Nota ruim')
+}
