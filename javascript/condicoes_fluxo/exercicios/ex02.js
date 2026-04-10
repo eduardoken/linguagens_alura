23810dae648b4907ae3d89ae7a88e5ef8edcf537
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-10 15:22:31 -0300

    exercicio 02

--- javascript/condicoes_fluxo/exercicios/ex02.js
+++ javascript/condicoes_fluxo/exercicios/ex02.js
@@ -1,1 +1,12 @@
+const horaAtual = 2
 
+if (horaAtual >=6 && horaAtual <=12) {
+    console.log('Bom dia')
+    
+} else if (horaAtual >=12 && horaAtual <=18) {
+    console.log('Boa tarde')
+    
+} else{
+    console.log('Boa noite')
+
+}
