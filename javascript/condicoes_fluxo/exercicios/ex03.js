65f97901becee2b8ec549dd91b41d5d1dad9c486
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-10 15:24:57 -0300

    exercicio 03

--- javascript/condicoes_fluxo/exercicios/ex03.js
+++ javascript/condicoes_fluxo/exercicios/ex03.js
@@ -1,1 +1,12 @@
+const numero = -8
 
+if (numero == 0) {
+    console.log('seu valor é zero')
+    
+} else if (numero > 0) {
+    console.log('seu valor é positivo')
+    
+} else {
+    console.log('seu valor é negativo')
+
+}
