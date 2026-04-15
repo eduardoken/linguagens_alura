1c74a33b2b9256e7e1976b8d05b06847a45fd001
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-15 15:05:31 -0300

    exercicio 09

--- javascript/condicoes_fluxo/exercicios/ex09.js
+++ javascript/condicoes_fluxo/exercicios/ex09.js
@@ -1,1 +1,16 @@
 
+const saldoDiponivel = 1200
+
+const compraAprovada = saldoDiponivel >= 300
+
+
+switch (compraAprovada) {
+    case true:
+        console.log('Compra aprovada');
+        
+        break;
+        
+    default:
+        console.log('Saldo insuficiente');
+        break;
+}
