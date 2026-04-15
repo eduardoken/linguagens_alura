d46d15f2035e709068a7bb93d0ecb13ef8dbb542
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-15 17:06:30 -0300

    exercicio 10

--- javascript/condicoes_fluxo/exercicios/ex10.js
+++ javascript/condicoes_fluxo/exercicios/ex10.js
@@ -1,1 +1,17 @@
 
+const nome = 'eu'
+const email = 'eu@eu'
+const idade = 21
+const formularioValido = true
+
+switch (formularioValido) {
+    case true:
+        console.log('Formulário enviado com sucesso');
+
+        break;
+
+    default:
+        console.log('Por favor, preencha todos os campos corretamente');
+        break;
+}
+
