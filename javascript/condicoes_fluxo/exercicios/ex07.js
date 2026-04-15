665c47c590a30c2ab3ddf48ef2a51d1cf4a76a95
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-15 14:52:31 -0300

    exercicio 07

--- javascript/condicoes_fluxo/exercicios/ex07.js
+++ javascript/condicoes_fluxo/exercicios/ex07.js
@@ -1,1 +1,13 @@
 
+const email = ''
+
+switch (email) {
+    case '':
+        console.log('Preencha o campo de e-mail');
+        
+        break;
+        
+    default:
+        console.log('E-mail válido');
+        break;
+}
