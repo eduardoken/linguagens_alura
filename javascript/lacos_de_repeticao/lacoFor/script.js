22884490829ff686b57f8be95db2a572a19d23e2
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-15 17:54:33 -0300

    for

---
+++ javascript/lacos_de_repeticao/lacoFor/script.js
@@ -0,0 +1,19 @@
+for(let contador = 1; contador <= 10;  contador++) {
+    console.log(contador)
+    
+}
+
+for(let numero = 0; numero <=15; numero++) {
+    if (numero % 2 == 0) {
+        console.log(numero)
+        
+    }
+}
+
+const palavra = 'calopsita'
+
+for(let contador = 0; contador < palavra.length; contador++) {
+    console.log(palavra[contador])
+    
+
+}
