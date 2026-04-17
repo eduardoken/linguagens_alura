5fc78e541fff96146fc0c7694f31c9ef0df158b9
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-17 17:45:42 -0300

    exercicio 10

---
+++ javascript/lacos_de_repeticao/exercicios/exercicio10.js
@@ -0,0 +1,35 @@
+// let opcao = 1
+
+// do {
+//     if (opcao == 1) {
+//         console.log('Ver saldo');
+        
+//     } else if (opcao == 2) {
+//         console.log('Fazer depósito');
+        
+//     } else {
+//         console.log('Sair');
+        
+//     }
+//     opcao++
+    
+// } while (opcao <= 3);
+
+let opcao = 1
+
+do {
+    if (opcao == 1) {
+        console.log('Ver saldo');
+        
+    } else if (opcao == 2) {
+        console.log('Fazer depósito');
+        
+    }   
+    opcao++
+    
+} while (opcao != 3);
+
+if (opcao == 3) {
+    console.log('Sair');
+    
+}
