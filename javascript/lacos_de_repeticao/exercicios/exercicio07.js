c2e86cfb2cb03968a7679ee641002d21606d60e7
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-17 17:16:11 -0300

    exercicio 07

---
+++ javascript/lacos_de_repeticao/exercicios/exercicio07.js
@@ -0,0 +1,8 @@
+const anoNascimento = 2005
+let anoAtual = 2026
+
+for (let idade = 0; idade + anoNascimento <= anoAtual; idade++) {
+console.log(idade, 'ano no ano de', anoNascimento + idade);
+
+
+}
