6ff03a30fd624151e1640a3343e5a516cd70707d
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-17 16:45:39 -0300

    exercicio 05

---
+++ javascript/lacos_de_repeticao/exercicios/exercicio05.js
@@ -0,0 +1,22 @@
+// let numeroDigitado = 9
+// let numerosContados = 0
+// do {
+//     numeroDigitado--
+//     numerosContados++
+    
+// } while (numeroDigitado != 0);
+// console.log(numeroDigitado);
+// console.log(numerosContados);
+
+let numeroDigitado = 9
+let numerosContados = 0
+do {
+    if (numeroDigitado != 0) {
+        numerosContados++
+    }   
+    numeroDigitado--
+    
+} while (numeroDigitado > 0);
+console.log(numeroDigitado);
+console.log(numerosContados);
+
