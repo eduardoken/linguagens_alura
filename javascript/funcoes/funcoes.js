6b8730a2745a0139df9becc5172a37b37eeb1831
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-21 14:17:45 -0300

    funções parte 2

--- javascript/funcoes/funcoes.js
+++ javascript/funcoes/funcoes.js
@@ -1,14 +1,28 @@
- function saudacao(nome) {
-    console.log('Olá', nome);
+//  function saudacao(nome) {
+//     console.log('Olá', nome);
     
- }
+//  }
+
+// const saudacao = (nome) => {
+//     console.log('Olá', nome)
+    
+// }
+
+const saudacao = nome => console.log('Olá', nome)
+
 
  saudacao('Eduardo')
 
 
- function calcularDobro(numero) {
-    return numero * 2
- }
+//  function calcularDobro(numero) {
+//     return numero * 2
+//  }
+
+// const calcularDobro = (numero) => {
+//     return numero * 2
+// }
+
+const calcularDobro = numero => numero * 2
 
  const numeroDobrado = calcularDobro(4)
  console.log(numeroDobrado)
