65293b66438917c025967f8dd9964f5991c28945
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-21 16:54:43 -0300

    funções globais

---
+++ javascript/funcoes/funcoes_globais.js
@@ -0,0 +1,17 @@
+function saudacao() {
+    console.log('E aí');
+    
+}
+
+// setTimeout(saudacao,1000) // ms 
+
+let contador = 0
+
+const id = setInterval(() => {
+    contador++
+    console.log('Tempo decorrido (em segundos):', contador)
+    if (contador ==10) {
+        clearInterval(id)
+        
+    }
+}, 1000)
