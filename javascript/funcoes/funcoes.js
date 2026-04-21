02437f8d9bf07047ae033c2609e90000ec0c6b76
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-21 14:07:04 -0300

    funções

---
+++ javascript/funcoes/funcoes.js
@@ -0,0 +1,15 @@
+ function saudacao(nome) {
+    console.log('Olá', nome);
+    
+ }
+
+ saudacao('Eduardo')
+
+
+ function calcularDobro(numero) {
+    return numero * 2
+ }
+
+ const numeroDobrado = calcularDobro(4)
+ console.log(numeroDobrado)
+ 
