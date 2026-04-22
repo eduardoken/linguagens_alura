2e87075f9a3e6c273e14b489fa35a9600ed4e897
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-22 19:48:04 -0300

    exercicio 10

---
+++ javascript/funcoes/exercicios/exercicio10.js
@@ -0,0 +1,12 @@
+function fazerPergunta(pergunta,respostaCorreta) {
+    let respostaUsuario = respostaCorreta
+    if (respostaUsuario == respostaCorreta) {
+        console.log('Respota correta');
+        
+    } else {
+        console.log('Respota incorreta');
+
+    }
+}
+
+fazerPergunta('Qual é a capital do Brasil?', 'Brasília')
