28a1b42f6545d877150a01fedfaa874d45b1ee84
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-22 18:06:51 -0300

    exercicio 05

---
+++ javascript/funcoes/exercicios/exercicio05.js
@@ -0,0 +1,13 @@
+function ehPar(numero) {
+    
+    if (numero % 2 == 0) {
+        return 'Numero par'
+        
+    } else {
+        return 'Numero impar'
+        
+    }
+}
+
+const resultado = ehPar(7)
+console.log(resultado);
