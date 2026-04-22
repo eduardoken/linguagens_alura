d1f6980849c7f9310befbfb330fa100ea2a83201
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-22 19:18:49 -0300

    retirando os teste

--- javascript/funcoes/exercicios/exercicio08.js
+++ javascript/funcoes/exercicios/exercicio08.js
@@ -2,8 +2,3 @@ const soma = (numero1 , numero2) => numero1 + numero2
 console.log(soma(2,3));
 
 
-const dobrar = numero => numero * 2
-
-const subtrair = (a, b) => a - b
-
-const saudacao = nome => 'Edu'
