32b81822da0be222569750ed4d701fbf0d9655f0
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-22 18:01:41 -0300

    exercicio 04

---
+++ javascript/funcoes/exercicios/exercicio04.js
@@ -0,0 +1,11 @@
+function verificarAprovacao(nota) {
+    if (nota >= 7) {
+        console.log('aprovado');
+        
+    } else {
+        console.log('Reprovado');
+        
+    }
+}
+
+verificarAprovacao(7)
