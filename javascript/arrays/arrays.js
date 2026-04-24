d1947671390a21ca902b63df2a4a93d8e7d7bc44
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-24 17:03:13 -0300

    arrays

---
+++ javascript/arrays/arrays.js
@@ -0,0 +1,9 @@
+const frutas = ['Uva','Banana','Kiwi','Maça','Morango']
+console.log(frutas)
+console.log(frutas.length)
+frutas.push('Melancia')
+console.log(frutas)
+console.log(frutas.length)
+console.log(frutas[0])
+console.log(frutas[4])
+console.log(frutas[frutas.length-1])
