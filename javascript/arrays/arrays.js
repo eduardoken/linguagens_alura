7ff816ca7b43540e13e424286876045d488bbe24
Eduardo Kenzo <edukenzo428@gmail.com>
2026-04-24 17:14:58 -0300

    arrays2

--- javascript/arrays/arrays.js
+++ javascript/arrays/arrays.js
@@ -1,9 +1,35 @@
 const frutas = ['Uva','Banana','Kiwi','Maça','Morango']
+
 console.log(frutas)
-console.log(frutas.length)
-frutas.push('Melancia')
-console.log(frutas)
-console.log(frutas.length)
-console.log(frutas[0])
-console.log(frutas[4])
-console.log(frutas[frutas.length-1])
+
+// console.log(frutas.length)
+// frutas.push('Melancia')
+// console.log(frutas)
+// console.log(frutas.length)
+// console.log(frutas[0])
+// console.log(frutas[4])
+// console.log(frutas[frutas.length-1])
+
+// frutas.splice(2,1)
+// console.log('Depois de remover:',frutas)
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log('indice:', i)
+    console.log(frutas[i])
+}
+
+console.log('ForEach')
+
+
+frutas.forEach((valor, indice) => {
+    console.log('índice:', indice, valor)
+    
+});
+
+console.log('Usando forOff:')
+
+for (const fruta of frutas) {
+ console.log('fruta da vez:', fruta);
+    
+    
+}
